6f16d644bc09d9e2059c45d10d7e287328dbd60c
Dante Fontana <dafontana@gmail.com>
2024-06-21 19:10:54 -0300

    Agregado una columna para la fecha

--- pedidos2.py
+++ pedidos2.py
@@ -1,52 +1,55 @@
 #!/usr/bin/env python
 """
 Lista de pedidos del taller
-AGREGAR FECHA. Puede ser despues del id
+AGREGAR PRECIO TOTAL para tener precio unitario y total
 """
 import json
 
 class Pedido:
-    def __init__(self, id, cliente, descripcion, cantidad, precio):
+    def __init__(self, id, cliente, descripcion, cantidad, precio, fecha):
         self.id = id
         self.cliente = cliente
         self.descripcion = descripcion
         self.cantidad = cantidad
         self.precio = precio
+        self.fecha = fecha
 
 class Taller:
     def __init__(self):
         self.pedidos = []
         self.next_id = 1
 
-    def agregar_pedido(self, cliente, descripcion, cantidad, precio):
-        pedido = Pedido(self.next_id, cliente, descripcion, cantidad, precio)
+    def agregar_pedido(self, cliente, descripcion, cantidad, precio, fecha):
+        pedido = Pedido(self.next_id, cliente, descripcion, cantidad, precio, fecha)
         self.pedidos.append(pedido)
         self.next_id += 1
     
     def listar_pedidos(self):
         # Encabezado de la tabla
-        header = "{:<6} {:<30} {:<70} {:<10} {:<10}".format("ID", "Cliente", "Descripción", "Cantidad", "Precio")
+        header = "{:<6} {:<30} {:<70} {:<10} {:<10} {:<20}".format("ID", "Cliente", "Descripción", "Cantidad", "Precio", "Fecha")
         print(header)
-        print("=" * 130)  # Línea divisoria
+        print("=" * 150)  # Línea divisoria
         
         # Filas de datos
         for pedido in self.pedidos:
-            row = "{:<6} {:<30} {:<70} {:<10} {:<10}".format(
+            row = "{:<6} {:<30} {:<70} {:<10} {:<10} {:<20}".format(
                 pedido.id,
                 pedido.cliente,
                 pedido.descripcion,
                 pedido.cantidad,
-                pedido.precio
+                pedido.precio,
+                pedido.fecha
             )
             print(row)
-        print("=" * 130)  # Línea divisoria
+        print("=" * 150)  # Línea divisoria
 
-    def editar_pedido(self, id, nueva_descripcion, nueva_cantidad, nuevo_precio):
+    def editar_pedido(self, id, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_fecha):
         for pedido in self.pedidos:
             if pedido.id == id:
                 pedido.descripcion = nueva_descripcion
                 pedido.cantidad = nueva_cantidad
                 pedido.precio = nuevo_precio
+                pedido.fecha = nueva_fecha
                 print("Pedido editado exitosamente.")
                 return
         print("ID de pedido no encontrado.")
@@ -71,7 +74,8 @@ class Taller:
                 "cliente": pedido.cliente,
                 "descripcion": pedido.descripcion,
                 "cantidad": pedido.cantidad,
-                "precio": pedido.precio
+                "precio": pedido.precio,
+                "fecha": pedido.fecha
             })
 
         with open(archivo, "w") as file:
@@ -82,7 +86,7 @@ class Taller:
             with open(archivo, "r") as file:
                 datos = json.load(file)
                 for dato in datos:
-                    self.agregar_pedido(dato["cliente"], dato["descripcion"], dato["cantidad"], dato["precio"])
+                    self.agregar_pedido(dato["cliente"], dato["descripcion"], dato["cantidad"], dato["precio"], dato["fecha"])
         except FileNotFoundError:
             pass
 
@@ -106,7 +110,8 @@ def main():
             cantidad = int(input("Ingrese la cantidad: "))
             # Si es necesario hacer operaciones matematicas con precio hay que definirlo como float.
             precio = (input("Ingrese el precio: "))
-            taller.agregar_pedido(cliente, descripcion, cantidad, precio)
+            fecha = (input("Ingrese la fecha: "))
+            taller.agregar_pedido(cliente, descripcion, cantidad, precio, fecha)
             print("Pedido agregado exitosamente.")
         
         elif opcion == "2":
@@ -117,7 +122,8 @@ def main():
             nueva_descripcion = input("Ingrese la nueva descripción: ")
             nueva_cantidad = int(input("Ingrese la nueva cantidad: "))
             nuevo_precio = float(input("Ingrese el nuevo precio: "))
-            taller.editar_pedido(id, nueva_descripcion, nueva_cantidad, nuevo_precio)
+            nueva_fecha = input("Ingrese la nueva fecha: ")
+            taller.editar_pedido(id, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_fecha)
         
         elif opcion == "4":
             ids_str = input("Ingrese los IDs de los pedidos a eliminar (separados por comas): ")
